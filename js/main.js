1c200e65719d00479d6fbb70067169ef5ce4fa7e
Cassie Lee <95990259+casslee8757@users.noreply.github.com>
2022-05-03 17:18:30 +1000

    js refactored using data id

--- js/main.js
+++ js/main.js
@@ -11,36 +11,27 @@ const onAdd = () => {
 
     const item = createItem(text)
     items.appendChild(item);
+    item.scrollIntoView({block: 'center'})
     input.value = '';
     input.focus()
 }
 
+let id = 0; 
 const createItem = (text) => {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id)
+    itemRow.innerHTML = `
+        <div class="item" >
+            <span class="item__name">${text}</span>
+            <button class="item__delete" >
+                <i class="fas fa-minus" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>
+    `;
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fas fa-minus"></i>'
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow)
-    })
-
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    id++
     return itemRow;
 
 }
@@ -55,5 +46,13 @@ input.addEventListener('keypress', (ev) => {
     }
 })
 
+items.addEventListener('click', ev => {
+    const id = ev.target.dataset.id
+    if(id){
+        const tobeDeleted = document.querySelector(`.item__row[data-id="${id}"]`)
+        tobeDeleted.remove()
+        
+    }
+})
 
 
